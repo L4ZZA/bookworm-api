e9c1d2ff8e643748b3211166e0b1df0d7bb56c6c
Pietro Lazzarotto <pietro.lazz@gmail.com>
2018-01-14 14:49:13 +0000

    user signs up

--- src/index.js
+++ src/index.js
@@ -7,19 +7,21 @@ import dotenv from "dotenv";
 import Promise from "bluebird";
 
 import auth from "./routes/auth";
+import users from "./routes/users";
 
 dotenv.config();
 const app = express();
 app.use(bodyParser.json());
-// ovverride builtin Promise librry with bluebird promise library
+// ovverride builtin Promise library with bluebird promise library
 mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGODB_URL);
 
 // What does it do?
 app.use("/api/auth", auth);
+app.use("/api/users", users);
 
 app.get("/*", (req, res) => {
 	res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(8080, ()=> console.log("Running on localhost:8080"));
+app.listen(8080, () => console.log("Running on localhost:8080"));
